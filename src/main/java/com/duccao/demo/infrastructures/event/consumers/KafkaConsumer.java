7105eb279d07e20643ca43d454fdd4fd20789787
comchieu3mon <minhduc.itus@gmail.com>
2025-08-06 19:22:34 +0700

    feat: added in ConsumerRecord processor

--- src/main/java/com/duccao/demo/infrastructures/event/consumers/KafkaConsumer.java
+++ src/main/java/com/duccao/demo/infrastructures/event/consumers/KafkaConsumer.java
@@ -1,64 +1,35 @@
 package com.duccao.demo.infrastructures.event.consumers;
 
 import com.duccao.demo.share.tracing.TraceMethodExecution;
-import org.apache.commons.collections4.CollectionUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.support.Acknowledgment;
 
 import java.time.Instant;
-import java.util.Map;
-import java.util.UUID;
 
 public abstract class KafkaConsumer<K, V> {
   private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-  public void consumeRecords(ConsumerRecords<K, V> records, Acknowledgment acks) {
+  public void consumeRecord(ConsumerRecord<K, V> record, Acknowledgment acks) {
     var receivedTimestamp = Instant.now();
     var channel = getChannel();
     var minTs = Long.MAX_VALUE;
     var maxTs = Long.MIN_VALUE;
-    var batchSize = CollectionUtils.size(records);
-
-    var recordAndPartitionBuilder = new StringBuilder();
-    for (var record : records) {
-      minTs = Math.min(minTs, record.timestamp());
-      maxTs = Math.max(maxTs, record.timestamp());
-      recordAndPartitionBuilder
-          .append(record.partition())
-          .append(':')
-          .append(extractCorrelationId(record));
-    }
-
-    var batchCorId = UUID.randomUUID().toString();
-    var mdcMap = createLoggingMdcProperties(records, batchCorId, receivedTimestamp.toString(), channel);
-    log.info(
-        "message=Consume Records, receivedAt={}, type={}, batchSize={}, batchCorId={}, minTs={}, maxTs={}, correlationIds={}",
-        receivedTimestamp, channel, batchSize, batchCorId, minTs, maxTs, recordAndPartitionBuilder);
+    String correlationId = extractCorrelationId(record);
 
     try {
-      processRecords(records);
+      log.info("message=Consume Records, receivedAt={}, type={}, minTs={}, maxTs={}, correlationIds={}",
+          receivedTimestamp, channel, minTs, maxTs, correlationId);
+      processRecord(record);
     } finally {
-      log.debug("message = Acknowledging events batchCorId = {}, correlationId = {}", recordAndPartitionBuilder,
-          batchCorId);
+      log.debug("message = Acknowledging events, correlationId = {}", correlationId);
       acks.acknowledge();
     }
   }
 
-  private Map<String, String> createLoggingMdcProperties(ConsumerRecords<K, V> records, String batchCorId,
-                                                         String receivedTimestamp, String channel) {
-    return Map.of(
-        "batchCorId", batchCorId,
-        "numOfRecords", String.valueOf(CollectionUtils.size(records)),
-        "receivedTimestamp", receivedTimestamp,
-        "channelId", channel
-    );
-  }
-
   @TraceMethodExecution
-  protected abstract void processRecords(ConsumerRecords<K, V> records);
+  protected abstract void processRecord(ConsumerRecord<K, V> record);
 
   protected abstract String extractCorrelationId(ConsumerRecord<K, V> record);
 
